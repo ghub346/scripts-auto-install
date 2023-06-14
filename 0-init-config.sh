9df0c4287dd63ad7478d628a6fc64132a0af2494
ghub346 <79888523+ghub346@users.noreply.github.com>
2023-06-14 14:52:56 -0500

    Update 0-init-config.sh
    Add user and group through script.

--- 0-init-config.sh
+++ 0-init-config.sh
@@ -1,1 +1,38 @@
 
+#!/bin/bash
+set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Simon Micheneau inspired of Erik Dubois
+# Website 	: 	https://www.simon-micheneau.fr/
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+# Create user
+
+useradd -s /bin/bash -m -G sudo,wheel baller175
+
+# Create wheel group.
+
+groupadd wheel
+
+# Create wheel entry in sudoers file.
+touch /etc/sudoers.d/wheelie ; echo "%wheel         ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers.d/wheelie
+
+
+
+
+###############################################################################################
+
+# installation of zippers and unzippers
+#sudo apt-get install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
+
+
+echo "################################################################"
+echo "##################       Init Config           #################"
+echo "################################################################"
