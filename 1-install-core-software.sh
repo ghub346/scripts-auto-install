b4485d5f2671f97268e646bf7ed3716b6c50c9a0
ghub346 <79888523+ghub346@users.noreply.github.com>
2023-06-14 12:31:12 -0500

    Update 1-install-core-software.sh
    Removed packages, not shown as available on Ubuntu 22.04 LTS version.

--- 1-install-core-software.sh
+++ 1-install-core-software.sh
@@ -12,7 +12,7 @@ set -e
 ##################################################################################################################
 
 #software from 'normal' repositories
-sudo apt-get install -y apt-transport-https openssl software-properties-common python-software-properties git curl ca-certificates zlib1g zlib1g-dev libpcre3 libpcre3-dev build-essential libssl-dev
+sudo apt-get install -y apt-transport-https openssl ca-certificates zlib1g libpcre3
 sudo apt-get install -y dconf-cli dconf-editor ppa-purge ffmpeg
 
 ###############################################################################################
