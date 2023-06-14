528e64a39e5c70abbb7cb020d1d6adbc70119709
ghub346 <79888523+ghub346@users.noreply.github.com>
2023-06-14 12:11:29 -0500

    Update install-tailscale.sh
    Added repo's and install command.

--- software/install-tailscale.sh
+++ software/install-tailscale.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Simon Micheneau
+# Website : 	https://www.simon-micheneau.fr/
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt update
+sudo apt-get -y install tailscale
+
+
+echo "################################################################"
+echo "#####################  Tailscale installed  ####################"
+echo "################################################################"
