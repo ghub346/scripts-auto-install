e60dfd4510603b3ab1826e95f7ec5697e4231ed9
ghub346 <79888523+ghub346@users.noreply.github.com>
2023-06-15 01:28:44 -0500

    Update install-docker.sh
    Updated for Focal

--- software/install-docker.sh
+++ software/install-docker.sh
@@ -14,12 +14,22 @@
 sudo apt-get remove docker docker-engine docker.io
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+## For Focal
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#sudo apt-get install -y docker-ce
+#sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
+#sudo chmod +x /usr/bin/docker-compose
 
 echo "################################################################"
 echo "######################  docker installed  ######################"
