9eeb403d99ee1ed081000b46d1bdfa24e9d59470
ghub346 <79888523+ghub346@users.noreply.github.com>
2023-07-03 20:26:37 -0500

    Update install-teleport.sh

--- software/install-teleport.sh
+++ software/install-teleport.sh
@@ -11,6 +11,8 @@
 
 # echo to /etc/sysctl.conf 
 
-#net.ipv6.conf.all.disable_ipv6=1
-#net.ipv6.conf.default.disable_ipv6=1
-#net.ipv6.conf.lo.disable_ipv6=1
+#echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.lo.disable_ipv6=1" >> /etc/sysctl.conf
+
+#sysctl -p
